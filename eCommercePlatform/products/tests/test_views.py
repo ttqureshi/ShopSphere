b8745822e930411aedd97cedae82d791af00a681
M. Tayyab Tahir Qureshi <109274085+ttqureshi@users.noreply.github.com>
2024-09-04 17:48:46 +0500

    Update test_views.py

--- eCommercePlatform/products/tests/test_views.py
+++ eCommercePlatform/products/tests/test_views.py
@@ -4,41 +4,6 @@ from django.contrib.auth.models import User
 from products.models import Product, Category, ReviewRating
 
 
-class TestProductsListingView(TestCase):
-
-    def setUp(self):
-        self.category = Category.objects.create(name="Electronics")
-        self.product1 = Product.objects.create(
-            name="Phone",
-            description="A smartphone",
-            category=self.category,
-            price=10000,
-            image_url="https://media.wisemarket.com.pk/variant/inventory_26455.webp",
-        )
-        self.product2 = Product.objects.create(
-            name="Laptop",
-            description="A laptop",
-            category=self.category,
-            price=100000,
-            image_url="https://cdn.ttgtmedia.com/rms/onlineimages/hp_elitebook_mobile.jpg",
-        )
-
-    def test_products_listing_view(self):
-        response = self.client.get(reverse("products:products-listing"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/products_listing.html")
-        self.assertContains(response, self.product1)
-        self.assertContains(response, self.product2)
-
-    def test_products_listing_view_with_category_filter(self):
-        response = self.client.get(
-            reverse("products:products-listing"), {"category": "Electronics"}
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.product1)
-        self.assertContains(response, self.product2)
-
-
 class TestProductDetailView(TestCase):
 
     def setUp(self):
@@ -61,26 +26,6 @@ class TestProductDetailView(TestCase):
         self.assertTemplateUsed(response, "products/product_detail.html")
         self.assertContains(response, self.product)
         self.assertContains(response, self.review.review)
-
-
-class TestProductSearchView(TestCase):
-
-    def setUp(self):
-        self.category = Category.objects.create(name="Electronics")
-        self.product = Product.objects.create(
-            name="Phone",
-            description="A smartphone",
-            category=self.category,
-            price=10000,
-            image_url="https://media.wisemarket.com.pk/variant/inventory_26455.webp",
-        )
-
-    def test_product_search_view(self):
-        response = self.client.get(reverse("products:search"), {"search": "smartphone"})
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/search.html")
-        self.assertIn(self.product, response.context["search_results"])
-        self.assertEqual(response.context["search_query"], "smartphone")
 
 
 class TestSubmitReviewView(TestCase):
